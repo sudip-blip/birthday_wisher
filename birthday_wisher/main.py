2a714046b45dc298da03a39e7b6db1a882061457
Sudip KC <65460694+sudip-blip@users.noreply.github.com>
2022-06-25 16:46:53 +0545

    Update main.py

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -16,7 +16,7 @@ file = pd.read_csv("birthdays.csv")
 file_dict = {(row["month"], row["day"]): row for (index, row) in file.iterrows()}
 if today_tuple  in file_dict: 
     birth_day_person=file_dict[today_tuple]
-    if time==7:
+    if time==17:
         with open(f"letter_templates\letter_{random.randint(1,3)}.txt", "r") as letter:
             message = letter.read().replace("[NAME]", birth_day_person["name"])
 
