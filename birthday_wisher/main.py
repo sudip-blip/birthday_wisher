cd427a4c158dbcdf1e7ece7c41517e4ca0b998c9
Sudip KC <65460694+sudip-blip@users.noreply.github.com>
2022-06-26 12:54:40 +0545

    Update main.py

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -16,16 +16,15 @@ file = pd.read_csv("birthdays.csv")
 file_dict = {(row["month"], row["day"]): row for (index, row) in file.iterrows()}
 if today_tuple  in file_dict: 
     birth_day_person=file_dict[today_tuple]
-    if time==7:
-        with open(f"letter_templates\letter_{random.randint(1,3)}.txt", "r") as letter:
-            message = letter.read().replace("[NAME]", birth_day_person["name"])
+    with open(f"letter_templates\letter_{random.randint(1,3)}.txt", "r") as letter:
+        message = letter.read().replace("[NAME]", birth_day_person["name"])
 
 
-            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(my_email, password)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs=birth_day_person["email"],
-                msg=f"Subject:Happy Birthday\n\n{message}".encode("utf-8"),
-            )
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birth_day_person["email"],
+            msg=f"Subject:Happy Birthday\n\n{message}".encode("utf-8"),
+        )
